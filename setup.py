746f5f7366bb87f9baa0464d85c403ccd01724c3
Chris Walsh <chris.is.rad@pm.me>
2024-10-22 06:45:14 -0400

    add random generator methods.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+reqs = ['numpy']
+
 setup(
     name='oddpy',
     version='0.0.1',
@@ -7,7 +9,12 @@ setup(
     author='Chris Walsh',
     author_email='chris.is.rad@pm.me',
     packages=find_packages(),
-    install_requires=[
-
-    ]
+    install_requires=reqs,
+    setup_requires=reqs,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.12'
 )
