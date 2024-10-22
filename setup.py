021463b7cf1a4fb266f726992664b42d474576fc
Chris Walsh <chris.is.rad@pm.me>
2024-10-22 07:20:59 -0400

    update version in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ reqs = ['numpy']
 
 setup(
     name='oddpy',
-    version='0.0.1',
+    version='0.0.3',
     description='a toolkit',
     author='Chris Walsh',
     author_email='chris.is.rad@pm.me',
